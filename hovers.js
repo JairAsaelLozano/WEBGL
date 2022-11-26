59f7fe967991f392f6a3b8ffe9730ec79668b865
JairAsaelLozano <asael.lozanofrr@uanl.edu.mx>
2022-11-25 22:13:52 -0600

    proyectonuevo

---
+++ hovers.js
@@ -0,0 +1,173 @@
+
+function onHoverJugar() {
+    
+    $("#jugar").attr('src', './imagenes//botones/jugar_h.png');
+}
+
+function offHoverJugar() {
+    
+    $("#jugar").attr('src', './imagenes//botones/jugar.png');
+}
+
+function onHoverMultijugador() {
+    
+    $("#multijugador").attr('src', './imagenes//botones/multijugador_h.png');
+}
+
+function offHoverMultijugador() {
+    
+    $("#multijugador").attr('src', './imagenes//botones/multijugador.png');
+}
+
+function onHoverOpciones() {
+    
+    $("#opciones").attr('src', './imagenes//botones/opciones_h.png');
+}
+
+function offHoverOpciones() {
+    
+    $("#opciones").attr('src', './imagenes//botones/opciones.png');
+}
+
+function onHoverPuntajes() {
+    
+    $("#puntajes").attr('src', './imagenes//botones/puntajes_h.png');
+}
+
+function offHoverPuntajes() {
+    
+    $("#puntajes").attr('src', './imagenes//botones/puntajes.png');
+}
+
+function onHoverSalir() {
+    
+    $("#salir").attr('src', './imagenes//botones/salir_h.png');
+}
+
+function offHoverSalir() {
+    
+    $("#salir").attr('src', './imagenes//botones/salir.png');
+}
+
+function onHoverIzquierda() {
+    
+    $("#regresar").attr('src', './imagenes//botones/izquierda_h.png');
+}
+
+function offHoverIzquierda() {
+    
+    $("#regresar").attr('src', './imagenes//botones/izquierda.png');
+}
+
+function onHovermenos() {
+    
+    $("#-").attr('src', './imagenes//botones/menos_h.png');
+}
+
+function offHovermenos() {
+    
+    $("#-").attr('src', './imagenes//botones/menos.png');
+}
+
+function onHovermas() {
+    
+    $("#mas").attr('src', './imagenes//botones/mas_h.png');
+}
+
+function offHovermas() {
+    
+    $("#mas").attr('src', './imagenes//botones/mas.png');
+}
+
+function onHoverPausa() {
+    
+    $("#pausa").attr('src', './imagenes//botones/pausa_h.png');
+}
+
+function offHoverPausa() {
+    
+    $("#pausa").attr('src', './imagenes//botones/pausa.png');
+}
+
+function onHoverRenaudar() {
+    
+    $("#reanudar").attr('src', './imagenes//botones/reanudar_h.png');
+}
+
+function offHoverRenaudar() {
+    
+    $("#reanudar").attr('src', './imagenes//botones/reanudar.png');
+}
+
+function onHoverSettings() {
+    
+    $("#settings").attr('src', './imagenes//botones/ajustes_h.png');
+}
+
+function offHoverSettings() {
+    
+    $("#settings").attr('src', './imagenes//botones/ajustes.png');
+}
+
+function onHoverRegresar() {
+    
+    $("#regresar").attr('src', './imagenes//botones/regresar_h.png');
+}
+    
+function offHoverRegresar() {
+    
+    $("#regresar").attr('src', './imagenes//botones/regresar.png');
+}
+
+
+//multijugador
+
+function onHoverPausaMulti() {
+    
+    $("#pausamulti").attr('src', '../imagenes//botones/pausa_h.png');
+}
+
+function offHoverPausaMulti() {
+    
+    $("#pausamulti").attr('src', '../imagenes//botones/pausa.png');
+}
+
+function onHoverRenaudarMulti() {
+    
+    $("#reanudarmulti").attr('src', '../imagenes//botones/reanudar_h.png');
+}
+
+function offHoverRenaudarMulti() {
+    
+    $("#reanudarmulti").attr('src', '../imagenes//botones/reanudar.png');
+}
+
+function onHoverSettingsMulti() {
+    
+    $("#settingsmulti").attr('src', '../imagenes//botones/ajustes_h.png');
+}
+
+function offHoverSettingsMulti() {
+    
+    $("#settingsmulti").attr('src', '../imagenes//botones/ajustes.png');
+}
+
+function onHoverRegresarMulti() {
+    
+    $("#regresarmulti").attrMulti('src', '../imagenes//botones/regresar_h.png');
+}
+    
+function offHoverRegresarMulti() {
+    
+    $("#regresarmulti").attr('src', '../imagenes//botones/regresar.png');
+}
+
+function onHoverSalirMulti() {
+    
+    $("#salirmulti").attr('src', '../imagenes//botones/salir_h.png');
+}
+
+function offHoverSalirMulti() {
+    
+    $("#salirmulti").attr('src', '../imagenes//botones/salir.png');
+}
---
+++ player.js
@@ -0,0 +1,204 @@
+class Player {
+ 
+    constructor(){
+        this.state = 1;
+        this.animations = [];    
+    }
+
+    get index(){
+        return this.state;
+    }
+
+    setIndex(value){
+        this.state = value;
+    }
+
+    get mixers(){
+        return this.animations;
+    }
+
+    setModel(modelName, myModel, xPos, yPos, zPos){
+        this.modelRef;
+        this.start;
+        this.stopped;
+        this.walk;
+        this.jump;
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+
+        this.maxJump = 4.6;
+        this.jumping = false;
+        this.isJump = false;
+
+        this.beggin = true;
+        this.countBeggin = 0;
+
+        var loader = new THREE.FBXLoader();
+ 		loader.load(modelName, function (object){
+			object.mixer = new THREE.AnimationMixer(object);
+
+			myModel.mixers.push(object.mixer);
+
+            myModel.start = object.mixer.clipAction(object.animations[10]);
+			myModel.stopped = object.mixer.clipAction(object.animations[9]);
+            myModel.walk = object.mixer.clipAction(object.animations[4]);
+            myModel.jump = object.mixer.clipAction(object.animations[2]);
+
+            myModel.start.play();
+			myModel.stopped.play();
+            myModel.walk.play();
+            myModel.jump.play();
+
+            myModel.start.weight = 1;
+            myModel.stopped.weight = 0;
+            myModel.walk.weight = 0;
+            myModel.jump.weight = 0;
+
+            object.position.x = xPos;
+			object.position.y = yPos;
+            object.position.z = zPos;
+            
+            myModel.modelRef = object;
+            
+			object.scale.set(4,4,4);
+            object.rotation.y = THREE.Math.degToRad(90);
+			
+			scene.add(object);
+		});
+    }
+
+    getIsJump(floor){
+        if(this.isJump){
+            if(this.jumping){
+                this.y += 0.1;
+                this.y = Math.round((this.y + Number.EPSILON) * 100) / 100;
+
+                if(this.y >= this.maxJump){
+                    this.jumping = false;
+                }
+            }else{
+                this.y -= 0.1;
+                this.y = Math.round((this.y + Number.EPSILON) * 100) / 100;
+
+                if(this.y <= floor){
+                    this.isJump = false;
+                }
+            }
+        }
+
+        return this.isJump;
+    }
+
+    getPosition(){
+        return this.modelRef.position;
+    }
+    getPositionx(){
+        return this.modelRef.position.x;
+    }
+    getPositionz(){
+        return this.modelRef.position.z;
+    }
+
+    playAnimation(playerMovX, playerMovY, playerMovZ){
+        switch(this.index){
+            case 0:{
+                this.start.weight = 1;
+                this.stopped.weight = 0;
+                this.walk.weight = 0;
+                this.jump.weight = 0;
+
+            }break;
+
+            case 1:{
+                this.start.weight = 0;
+                this.stopped.weight = 1;
+                this.walk.weight = 0;
+                this.jump.weight = 0;
+
+            }break;
+
+            case 2:{
+                this.start.weight = 0;
+                this.stopped.weight = 0;
+                this.walk.weight = 1;
+                this.jump.weight = 0;
+
+                this.modelRef.position.x += playerMovX*deltaTime;
+                this.modelRef.position.z += playerMovZ*deltaTime;
+
+                if(playerMovX > 0 && playerMovZ == 0){
+                    this.modelRef.rotation.y = THREE.Math.degToRad(90);
+                }
+                else if(playerMovZ == 0){
+                    this.modelRef.rotation.y = THREE.Math.degToRad(-90);
+                }
+                if(playerMovZ > 0 && playerMovX == 0){
+                    this.modelRef.rotation.y = THREE.Math.degToRad(0);
+                }
+                else if (playerMovX == 0) {
+                    this.modelRef.rotation.y = THREE.Math.degToRad(180);
+                }
+
+            }break;
+
+            case 3:{
+                this.start.weight = 0;
+                this.stopped.weight = 1;
+                this.walk.weight = 1;
+                this.jump.weight = 0;
+
+                this.modelRef.position.x += playerMovX*deltaTime;
+
+                if(playerMovX > 0){
+                    this.modelRef.rotation.y = THREE.Math.degToRad(90);
+                }
+                else{
+                    this.modelRef.rotation.y = THREE.Math.degToRad(-90);
+                }
+
+            }break;
+
+            case 4:{
+                this.start.weight = 0;
+                this.stopped.weight = 0;
+                this.walk.weight = 0;
+                this.jump.weight = 1;
+
+                this.modelRef.position.x += playerMovX*deltaTime;
+                if(this.jumping){
+                    this.modelRef.position.y += 8*deltaTime;
+                }else{
+                    this.modelRef.position.y -= 8*deltaTime;   
+                }
+                //this.modelRef.position.y += playerMovY*deltaTime;
+                this.modelRef.position.y = Math.round((this.modelRef.position.y + Number.EPSILON) * 10) / 10;
+
+                //console.log(this.modelRef.position.y);
+
+
+                if(playerMovX > 0){
+                    this.modelRef.rotation.y = THREE.Math.degToRad(90);
+                }
+                else if(playerMovX < 0){
+                    this.modelRef.rotation.y = THREE.Math.degToRad(-90);
+                }
+
+            }break;
+
+        }
+
+
+        if (this.mixers.length > 0) {
+            for (var i = 0; i < this.mixers.length; i++) {
+                this.mixers[i].update(deltaTime);
+                if(this.beggin){
+                    this.countBeggin += 1;
+                    if(this.countBeggin > 10100*deltaTime){
+                        this.beggin = false;
+                    }
+                }
+            }
+        }
+    }
+}
